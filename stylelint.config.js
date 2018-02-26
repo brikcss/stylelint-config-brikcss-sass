597880e2e4b2ee85661e16bad6ffff54a12fabd8
The Zimmee <thezimmee@gmail.com>
2018-02-26 15:24:43 -0700

    fix(config): Add some `stylelint-config-css overrides` for common issues that prettier handles.
    [BRANCH: master]

--- stylelint.config.js
+++ stylelint.config.js
@@ -7,7 +7,6 @@
  * ---------------------------------------------------------------------
  */
 
-
 /* eslint-env node */
 var isProd = process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production';
 module.exports = {
@@ -16,41 +15,47 @@ module.exports = {
 		'stylelint-config-recommended',
 		'@brikcss/stylelint-config-css'
 	],
-	plugins: [
-		'stylelint-scss',
-	],
+	plugins: ['stylelint-scss'],
 	formatter: 'stylelint-formatter-pretty',
 	processors: [],
 	ignoreFiles: [],
 	defaultSeverity: isProd ? 'error' : 'warning',
 	rules: {
-			/** OVERWRITE EXISTING stylelint-config-css CSS RULES. */
-				'max-nesting-depth': [3,
-					{
-						ignore: ['blockless-at-rules'],
-						// ignoreAtRules: []
-					}
-				],
-			/** SASS RULES (stylelint-scss). */
-				'scss/at-else-if-parentheses-space-before': 'always',
-				'scss/at-function-parentheses-space-before': 'never',
-				'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
-				'scss/at-import-partial-extension-blacklist': ['css', 'scss', 'sass', 'less', 'styl', 'stylus'],
-				'scss/at-mixin-parentheses-space-before': 'never',
-				// 'scss/at-mixin-pattern': '/foo-.+/',
-				// 'scss/at-function-pattern': '/foo-.+/',
-				'scss/at-rule-no-unknown': true,
-				'scss/dollar-variable-colon-space-after': 'always',
-				'scss/dollar-variable-colon-space-before': 'never',
-				'scss/dollar-variable-empty-line-before': ['always', {except: ['first-nested', 'after-dollar-variable'], ignore: ['after-comment']}],
-				'scss/dollar-variable-no-missing-interpolation': true,
-				// 'scss/dollar-variable-pattern': '/foo-.+/',
-				// 'scss/percent-placeholder-pattern': '/foo-.+/',
-				// 'scss/percent-placeholder-pattern': '/foo-.+/',
-				'scss/media-feature-value-dollar-variable': 'always',
-				'scss/operator-no-newline-after': true,
-				'scss/operator-no-newline-before': true,
-				'scss/operator-no-unspaced': true,
-				'scss/selector-no-redundant-nesting-selector': true,
+		/** OVERWRITE EXISTING stylelint-config-css CSS RULES. */
+		'max-nesting-depth': [
+			3,
+			{
+				ignore: ['blockless-at-rules']
+				// ignoreAtRules: []
+			}
+		],
+		indentation: null,
+		'function-comma-space-after': null,
+		'function-parentheses-newline-inside': null,
+
+		/** SASS RULES (stylelint-scss). */
+		'scss/at-else-if-parentheses-space-before': 'always',
+		'scss/at-function-parentheses-space-before': 'never',
+		'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
+		'scss/at-import-partial-extension-blacklist': ['css', 'scss', 'sass', 'less', 'styl', 'stylus'],
+		'scss/at-mixin-parentheses-space-before': 'never',
+		// 'scss/at-mixin-pattern': '/foo-.+/',
+		// 'scss/at-function-pattern': '/foo-.+/',
+		'scss/at-rule-no-unknown': true,
+		'scss/dollar-variable-colon-space-after': 'always',
+		'scss/dollar-variable-colon-space-before': 'never',
+		'scss/dollar-variable-empty-line-before': [
+			'always',
+			{ except: ['first-nested', 'after-dollar-variable'], ignore: ['after-comment'] }
+		],
+		'scss/dollar-variable-no-missing-interpolation': true,
+		// 'scss/dollar-variable-pattern': '/foo-.+/',
+		// 'scss/percent-placeholder-pattern': '/foo-.+/',
+		// 'scss/percent-placeholder-pattern': '/foo-.+/',
+		'scss/media-feature-value-dollar-variable': 'always',
+		'scss/operator-no-newline-after': true,
+		'scss/operator-no-newline-before': true,
+		'scss/operator-no-unspaced': true,
+		'scss/selector-no-redundant-nesting-selector': true
 	}
 };
